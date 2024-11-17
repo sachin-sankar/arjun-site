9d797babe69627b205abfa632d7b1edc27680939
Dharani I S <dharaniis@MacBook-Pro.local>
2024-11-17 18:58:28 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/home", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/fatloss", (req, res) => {
+    res.render("fatloss.ejs");
+});
+
+
+app.get("/bulking", (req, res) => {
+    res.render("bulking.ejs");
+});
+
+app.get("/bodyRecomp", (req, res) => {
+    res.render("bodyRecomp.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
+app.listen(port, () => {
+console.log(`Listening on port ${port}`);
+});
